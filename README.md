3f2aa880e3a5eb7f8e7878505bb46e031770166b
dail8859 <dail8859@yahoo.com>
2017-03-25 12:54:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# SurroundSelection
+Notepad++ plugin to automatically surround the selection in quotes/brackets/parenthesis/etc.
+
+**Note:** This is still in early development. It has not been tested with non-US keyboard layouts.
+
+## Usage
+Select some text and type one of the following characters:
+- `'`
+- `"`
+- `(`
+- `{`
+- `[`
+
+## Installation
+No public release is currently available.
+
+## Development
+The code has been developed using MSVC 2013. Building the code will generate the DLL which can be used by Notepad++. For convenience, MSVC copies the DLL into the Notepad++ plugin directory.
+
+## License
+This code is released under the [GNU General Public License version 2](http://www.gnu.org/licenses/gpl-2.0.txt).
---
+++ src/Main.cpp
@@ -0,0 +1,197 @@
+// This file is part of SurroundSelection.
+// 
+// Copyright (C)2017 Justin Dailey <dail8859@yahoo.com>
+// 
+// SurroundSelection is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+#include "PluginInterface.h"
+#include "ScintillaGateway.h"
+#include "resource.h"
+#include "AboutDialog.h"
+
+#include <algorithm>
+#include <vector>
+
+static HANDLE _hModule;
+static NppData nppData;
+static HHOOK hook = NULL;
+static bool hasFocus = true;
+static ScintillaGateway editor;
+
+static void showAbout();
+
+FuncItem funcItem[] = {
+	{ TEXT("About..."), showAbout, 0, false, nullptr }
+};
+
+static void showAbout() {
+	ShowAboutDialog((HINSTANCE)_hModule, MAKEINTRESOURCE(IDD_ABOUTDLG), nppData._nppHandle);
+}
+
+static HWND GetCurrentScintilla() {
+	int which = 0;
+	SendMessage(nppData._nppHandle, NPPM_GETCURRENTSCINTILLA, SCI_UNUSED, (LPARAM)&which);
+	return (which == 0) ? nppData._scintillaMainHandle : nppData._scintillaSecondHandle;
+}
+
+static void SurroundSelectionsWith(char ch1, char ch2) {
+	std::vector<std::pair<int, int>> selections;
+
+	int num = editor.GetSelections();
+	for (int i = 0; i < num; ++i) {
+		int start = editor.GetSelectionNStart(i);
+		int end = editor.GetSelectionNEnd(i);
+
+		if (start != end /* && editor.LineFromPosition(start) == editor.LineFromPosition(end) */)
+			selections.push_back(std::make_pair(start, end));
+	}
+
+	// Sort so they are replaced top to bottom
+	std::sort(selections.begin(), selections.end());
+
+	int offset = 0;
+	int i = 0;
+	editor.BeginUndoAction();
+	for (const auto &selection : selections) {
+		editor.SetTargetRange(selection.first + offset, selection.second + offset);
+
+		std::string target(editor.GetTargetText(NULL), '\0');
+		editor.GetTargetText(&target[0]);
+
+		// Add in the characters
+		target.insert(target.begin(), 1, ch1);
+		target.insert(target.end(), 1, ch2);
+
+		editor.ReplaceTarget(-1, target.c_str());
+
+		editor.SetSelectionNStart(i, selection.first + offset + 1);
+		editor.SetSelectionNEnd(i, selection.second + offset + 2 - 1);
+
+		offset += 2;
+		++i;
+	}
+
+	editor.EndUndoAction();
+}
+
+LRESULT CALLBACK KeyboardProc(int ncode, WPARAM wparam, LPARAM lparam) {
+	if (ncode == HC_ACTION) {
+		if ((HIWORD(lparam) & KF_UP) == 0) {
+			if (hasFocus && !(GetKeyState(VK_CONTROL) & KF_UP) && !(GetKeyState(VK_MENU) & KF_UP)) {
+				char ch1 = 0, ch2 = 0;
+
+				if (wparam == VK_OEM_7) {
+					if (GetKeyState(VK_SHIFT) & KF_UP) {
+						ch1 = ch2 = '"';
+					}
+					else {
+						ch1 = ch2 = '\'';
+					}
+				}
+				else if (wparam == VK_OEM_4) {
+					if (GetKeyState(VK_SHIFT) & KF_UP) {
+						ch1 = '{';
+						ch2 = '}';
+					}
+					else {
+						ch1 = '[';
+						ch2 = ']';
+					}
+				}
+				else if (wparam == 0x39) {
+					if (GetKeyState(VK_SHIFT) & KF_UP) {
+						ch1 = '(';
+						ch2 = ')';
+					}
+				}
+				//else if (wparam == VK_OEM_COMMA) {
+				//	if (GetKeyState(VK_SHIFT) & KF_UP) {
+				//		ch1 = '<';
+				//		ch2 = '>';
+				//	}
+				//}
+
+				if (ch1 != 0 && editor.GetSelectionEmpty() == 0) {
+					SurroundSelectionsWith(ch1, ch2);
+					return TRUE; // This key has been "handled" and won't propogate
+				}
+			}
+		}
+	}
+	return CallNextHookEx(hook, ncode, wparam, lparam); //pass control to next hook in the hook chain.
+}
+
+
+BOOL APIENTRY DllMain(HANDLE hModule, DWORD  reasonForCall, LPVOID lpReserved) {
+	switch (reasonForCall) {
+	case DLL_PROCESS_ATTACH:
+		_hModule = hModule;
+		break;
+	case DLL_PROCESS_DETACH:
+		break;
+	case DLL_THREAD_ATTACH:
+		break;
+	case DLL_THREAD_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+extern "C" __declspec(dllexport) void setInfo(NppData notepadPlusData) {
+	nppData = notepadPlusData;
+
+	// Set this as early as possible so its in a valid state
+	editor.SetScintillaInstance(nppData._scintillaMainHandle);
+}
+
+extern "C" __declspec(dllexport) const wchar_t *getName() {
+	return TEXT("SurroundSelection");
+}
+
+extern "C" __declspec(dllexport) FuncItem *getFuncsArray(int *nbF) {
+	*nbF = sizeof(funcItem) / sizeof(funcItem[0]);;
+	return funcItem;
+}
+
+extern "C" __declspec(dllexport) void beNotified(SCNotification *notifyCode) {
+	switch (notifyCode->nmhdr.code) {
+		case SCN_FOCUSIN:
+			hasFocus = true;
+			break;
+		case SCN_FOCUSOUT:
+			hasFocus = false;
+			break;
+		case NPPN_READY:
+			hook = SetWindowsHookEx(WH_KEYBOARD, KeyboardProc, (HINSTANCE)_hModule, ::GetCurrentThreadId());
+			break;
+		case NPPN_SHUTDOWN:
+			if (hook != NULL) UnhookWindowsHookEx(hook);
+			break;
+		case NPPN_BUFFERACTIVATED:
+			editor.SetScintillaInstance(GetCurrentScintilla());
+			break;
+	}
+	return;
+}
+
+extern "C" __declspec(dllexport) LRESULT messageProc(UINT Message, WPARAM wParam, LPARAM lParam) {
+	return TRUE;
+}
+
+#ifdef UNICODE
+extern "C" __declspec(dllexport) BOOL isUnicode() {
+	return TRUE;
+}
+#endif //UNICODE
